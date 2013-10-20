6380681b983a380eec0329515f23ec5d4dad27b3
peter942 <petoriaomalley@gmail.com>
2013-10-20 03:31:45 +0100

    changed css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131020011404) do
+ActiveRecord::Schema.define(version: 20131020015247) do
+
+  create_table "blogs", force: true do |t|
+    t.string   "title"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
